a846095c0e5bbf5550510b6d632aef8650b0f9b7
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-02-11 12:22:15 -0500

    Project Complete

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.businesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,12 +20,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,10 +46,81 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun BusinessCard() {
-
+    NameBrand()
+    Contact()
 }
+
+@Composable
+fun NameBrand(modifier: Modifier = Modifier) {
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val squareSize = screenWidth / 3
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(squareSize)
+                .background(Color.Black),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.android_logo),
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+        }
+        Text(
+            text = stringResource(R.string.brand_name),
+            fontSize = 56.sp,
+            modifier = Modifier
+        )
+        Text(
+            text = stringResource(R.string.android_developer_extraordinaire),
+            color = Color(0xFF032B0F)
+
+        )
+    }
+}
+
+@Composable
+fun Contact(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Row {
+            Text(
+                text = "\u260E (123) 456 - 7890",
+                fontSize = 24.sp
+            )
+        }
+        Row {
+            Text(
+                text = "\uD83D\uDD17 @AndroidRocks",
+                fontSize = 24.sp
+            )
+        }
+        Row {
+            Text(
+                text = "\u2709 john.doe@androidrocks.com",
+                fontSize = 24.sp
+            )
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
